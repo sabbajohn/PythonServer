74939133b05688407f83a97e2a54aded0fd3cf69
sabba.john <victorsabba@gmail.com>
2019-10-08 14:51:03 -0300

    Faltou uma aspa

--- Validador.py
+++ Validador.py
@@ -87,7 +87,7 @@ def query_generator(data):
         for item in data:
             if item['status']==True:
                 message = 'Verificado via API através do codigo {0} em {1}'.format(item['result']['comprovante_emitido'], item['result']['comprovante_emitido_data'])
-                f.write("UPDATE cliente SET id_status='1', nome = '{0}' , motivo ='{1}'  WHERE CPFCNPJ = {2};\n".format(item['result']['nome_da_pf'],message,item['result']['numero_de_cpf']))#Gerar query caso o TRUE
+                f.write("UPDATE cliente SET id_status='1', nome = '{0}' , motivo ='{1}'  WHERE CPFCNPJ = '{2}';\n".format(item['result']['nome_da_pf'],message,item['result']['numero_de_cpf']))#Gerar query caso o TRUE
             elif item['status']==False:
                 if item['code'] == 1:
                     f.write("UPDATE cliente SET id_status=2, motivo = '{0}' WHERE id = {1};\n".format(item['message'],item['id']))#Gerar query caso o TRUE
