9ea448f3a1403725eb4e13c9e96683b92c025d14
sabba.john <victorsabba@gmail.com>
2019-10-08 16:09:03 -0300

    Servidor API organizado em classes

--- server.py
+++ server.py
@@ -4,64 +4,34 @@ from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
 
-db_connect = create_engine('sqlite:///chinook.db')
-app = Flask(__name__)
-api = Api(app)
 
 
-class Employees(Resource):
-    def get(self):
-        conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from employees") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
 
+
+class Clientes(Resource):
+    
     def post(self):
-        conn = db_connect.connect()
+        #TODO: Inserir validação ṕor token....
         print(request.json)
-        LastName = request.json['LastName']
-        FirstName = request.json['FirstName']
-        Title = request.json['Title']
-        ReportsTo = request.json['ReportsTo']
-        BirthDate = request.json['BirthDate']
-        HireDate = request.json['HireDate']
-        Address = request.json['Address']
-        City = request.json['City']
-        State = request.json['State']
-        Country = request.json['Country']
-        PostalCode = request.json['PostalCode']
-        Phone = request.json['Phone']
-        Fax = request.json['Fax']
-        Email = request.json['Email']
-        query = conn.execute("insert into employees values(null,'{0}','{1}','{2}','{3}', \
-                             '{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}', \
-                             '{13}')".format(LastName,FirstName,Title,
-                             ReportsTo, BirthDate, HireDate, Address,
-                             City, State, Country, PostalCode, Phone, Fax,
-                             Email))
-        return {'status':'success'}
+        Nome = request.json['Nome']
+        CPFCNPJ= request.json['CPFCNPJ']
+        Dtnascimento= request.json['Dtnascimento']
+        # manda os dados dessa requisição pra uma estrutura de dados
+        return {'status':'success','message':'Cliente inserido na fila para validação de dados1'}
 
 
-class Tracks(Resource):
-    def get(self):
-        conn = db_connect.connect()
-        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return jsonify(result)
 
 
-class Employees_Name(Resource):
-    def get(self, employee_id):
-        conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return jsonify(result)
 
+class ApiSetting(object):
+    def __init__(self):
+        
+        self.app = Flask(__name__)
+        self.api = Api(self.app)
+        self.api.add_resource(Clientes, '/fila') # Route_1
 
-api.add_resource(Employees, '/employees') # Route_1
-api.add_resource(Tracks, '/tracks') # Route_2
-api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
-
-
+        self.runner(self.app)
+    def runner(self, app):
+        app.run(port=4444)
 if __name__ == '__main__':
-     app.run(port=4444)
-
+    test= ApiSetting()     
