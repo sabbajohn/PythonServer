6f2fddd8a13d76a97db4ae89593f26aa83a8da3d
sabba.john <victorsabba@gmail.com>
2019-11-04 12:42:40 -0300

    Classe de Banco de dados

--- Class/db.py
+++ Class/db.py
@@ -1,13 +1,25 @@
+#!/usr/bin/python3
+# coding: utf-8
+import sys
+import os
 import mysql.connector
+import logging
 class DB(object):
-    def __init__(self):
-        self.mydb = mysql.connector.connect(
-        host="localhost",
-        user="objetiva",
-        passwd="spqQVJ161",
-        database="megasorte"
-        )
-        self.handler()
-    
-    def handler(self):
-        return self.mydb
+	def __init__(self):
+		try:
+			log = logging.getLogger('Banco de Dados')
+			log.info('iniciando conexão com Banco de Dados.')
+			self.mydb = mysql.connector.connect(
+			host="10.255.237.4",
+			user="bwadmin",
+			passwd="8bNmFLiIPhVRrM",
+			database="megasorte"
+    	    )
+			log.info('Conexão esstabelecida com Sucesso!')
+			self.handler()
+		except mysql.connector.Error as err:
+			log.info('Erro ao conecar com o Banco de Dados.')
+			sys.exit("[!]Não foi possivel conectar a base de dados! Erro {}".format(err))  
+		
+	def handler(self):
+		return self.mydb
