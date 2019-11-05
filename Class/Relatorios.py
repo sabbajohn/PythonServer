3af71da2f04251e6ef69c7beb215acaa48728e79
sabba.john <victorsabba@gmail.com>
2019-11-05 13:58:57 -0300

    Inserir novos dados no banco

--- Class/Relatorios.py
+++ Class/Relatorios.py
@@ -22,6 +22,7 @@ class Relatorios(object):
 							print("[!] Tentando atualizar a base de dados!")
 							
 							db.execute("UPDATE cliente SET id_status='1', nome = '{0}' , motivo ='{1}'  WHERE CPFCNPJ = '{2}';\n".format(item['result']['nome_da_pf'],message,item['result']['numero_de_cpf']))
+							handler.mydb.commit()
 						except:
 							print("[!][!][!] Não foi possivel, mas voce pode efutar a atualização manualmente através do arquivo query.txt!")
 							pass
@@ -35,6 +36,7 @@ class Relatorios(object):
 								try:
 									print("[!] Tentando atualizar a base de dados!")
 									db.execute("UPDATE cliente SET id_status='2', motivo = '{0}' WHERE id = {1};\n".format(item['message'],item['id']))
+									handler.mydb.commit()
 								except:
 									print("[!][!][!] Não foi possivel, mas voce pode efutar a atualização manualmente através do arquivo query.txt!")
 									pass
@@ -45,6 +47,7 @@ class Relatorios(object):
 								try:
 									print("[!] Tentando atualizar a base de dados!")
 									db.execute("UPDATE cliente SET id_status='2', motivo = '{0}' WHERE id = {1};\n".format(item['message'],item['id']))
+									handler.mydb.commit()
 								except:
 									print("[!][!][!] Não foi possivel, mas voce pode efutar a atualização manualmente através do arquivo query.txt!")
 									pass
@@ -55,18 +58,21 @@ class Relatorios(object):
 								try:
 									print("[!] Tentando atualizar a base de dados!")
 									db.execute("UPDATE cliente SET id_status='2', motivo = '{0}' WHERE id = {1};\n".format(item['message'],item['id']))
+									handler.mydb.commit()
 								except:
 									print("[!][!][!] Não foi possivel, mas voce pode efutar a atualização manualmente através do arquivo query.txt!")
 									pass
 								
 
 								f.write("UPDATE cliente SET id_status='2', motivo = '{0}' WHERE id = {1};\n".format(item['message'],item['id']))
+								handler.mydb.commit()
 						except:
 							if item['return']=='NOK':
 								if "CPF Nao Encontrado na Base de Dados Federal." in item['message']:
 									try:
 										print("[!] Tentando atualizar a base de dados!")
 										db.execute("UPDATE cliente SET id_status='3', motivo = '{0}' WHERE CPFCNPJ = {1};\n".format(item['message'],item['CPF']))
+										handler.mydb.commit()
 									except:
 										print("[!][!][!] Não foi possivel, mas voce pode efutar a atualização manualmente através do arquivo query.txt!")
 										pass
@@ -76,6 +82,7 @@ class Relatorios(object):
 									try:
 										print("[!] Tentando atualizar a base de dados!")
 										db.execute("UPDATE cliente SET id_status='2', motivo = '{0}' WHERE CPFCNPJ = {1};\n".format(item['message'],item['CPF']))
+										handler.mydb.commit()
 									except:
 										print("[!][!][!] Não foi possivel, mas voce pode efutar a atualização manualmente através do arquivo query.txt!")
 										pass
@@ -85,6 +92,7 @@ class Relatorios(object):
 								elif  "Token Inválido ou sem saldo para a consulta." in item['message'] :
 									sys.exit(item['message'])	
 						else:
+							handler.mydb.commit()
 							pass
 
 	def responses(responses):
