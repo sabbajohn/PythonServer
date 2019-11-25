0bb18ea2d8abf448a3a401213f8d006a28b8aa58
sabba.john <victorsabba@gmail.com>
2019-11-25 11:58:38 -0300

    Classe no lugar do array

--- controle.py
+++ controle.py
@@ -1,147 +1,175 @@
 #!/usr/bin/python3
 # coding= utf-8
-from Initialize import Initialize
 
-class Controle(Initialize):
-	def __init__(self):
-		
-		self.Config 
-		self.Config_ENV 
-	
-	class Key(Controle):
-		def __init__(self,Controle):
-			self.Controle = Controle
 
-			env =self.Config.get("KEY","env")
-			root=self.Config.get("KEY","root")
-			user=self.Config.get("KEY","user")
+class Controle(object):
+	def __init__(self,I):
+		self.Config =I.Config
+		self.Config_ENV = I.Config_ENV
 		
-		
-	class DB():
-		def __init__(self,Controle):
-			self.Controle = Controle
-		class MSQL_R:
-			def __init__(self):
-				host=self.Config_ENV.get("MSQL_R","host")
-				user=self.Config_ENV.get("MSQL_R","user")
-				passwd=self.Config_ENV.get("MSQL_R","passwd")
-				database=self.Config_ENV.get("MSQL_R","database")
-				raise_on_warnings=self.Config_ENV.get("MSQL_R","raise_on_warnings")
-			
-				
-						
-		class MSQL_W:
-			def __init__(self):
-				host=self.Config_ENV.get("MSQL_W","host")
-				user=self.Config_ENV.get("MSQL_W","user")
-				passwd=self.Config_ENV.get("MSQL_W","passwd")
-				database=self.Config_ENV.get("MSQL_W","database")
-				raise_on_warnings=self.Config_ENV.get("MSQL_W","raise_on_warnings")
-			
-				
-				
-	class API:
-		def __init__(self,Controle):
-			self.Controle = Controle
-			
-		class mandrill:
-			def __init__(self):
-				api_key=self.Config_ENV.get("MANDRILL","api_key")
-		
-		class hubd:
-			def __init__(self):
-				url=self.Config_ENV.get("HUBD","url")
-				api_key=self.Config_ENV.get("HUBD","api_key")
-		
-		class soa:
-			def __init__(self):
-				url=self.Config_ENV.get("SOA","url")
-				user=self.Config_ENV.get("SOA","user")
-				key=self.Config_ENV.get("SOA","key")
-			
-		class comtele:
-			def __init__(self):
-				api_key=self.Config_ENV.get("COMTELE","api_key")
-				
+		self.Key = Key(self)
+		self.DB = DB(self)
+		self.API= API(self)
+		self.LINK = LINK(self)
+		self.logs=logs(self)
+		self.files=files(self)
+		self.modulos = modulos(self)
+ 
 	
-				
-	class LINK:
-		def __init__(self,Controle):
-			self.Controle = Controle
-			link_site=self.Config_ENV.get("LINKS","link_site")
-			link_de_compra=self.Config_ENV.get("LINKS","link_de_compra")
-			contact_mail=self.Config_ENV.get("LINKS","contact_mail")
-			
+class Key(Controle):
+
+	def __init__(self,Controle):
 		
-	class logs:
-		def __init__(self,Controle):
-			self.Controle = Controle
-			manager_log=self.Config.get("LOGS","manager_log")
-			sdu_log=self.Config.get("LOGS","sdu_log")
-			svc_log=self.Config.get("LOGS","svc_log")
-			sms_log=self.Config.get("LOGS","sms_log")
-			api_log=self.Config.get("LOGS","api_log")
-			startup_log=self.Config.get("LOGS","startup_log")
-			watch_log=self.Config.get("LOGS","watch_log")
 		
-	class files:
-		def __init__(self,Controle):
-			self.Controle = Controle
-			query=self.Config.get("FILES","query")
-			responses=self.Config.get("FILES","responses")
-			responses_api=self.Config.get("FILES","responses_api")
-			responses_sms=self.Config.get("FILES","responses_sms")
 	
-	class modulos:
+		
+		self.env  =Controle.Config.get("KEY","env")
+		self.root =Controle.Config.get("KEY","root")
+		self.user =Controle.Config.get("KEY","user")
+		
+		
+class DB(Controle):
+
+	def __init__(self,Controle):
+		
+
+		self.MYSQL_R = self.MYSQL_R(Controle)
+		self.MYSQL_W = self.MYSQL_W(Controle)
+	class MYSQL_R(object):
+		
 		def __init__(self,Controle):
-			self.Controle = Controle
-			class SMS:
-				def __init__(self, modulos):
-					self.moulos = modulos
-					init			= self.Config.getboolean("SMS","sms_init")
-					init_time			=None
-					delay				=None
-					keepAlive			= True
-					lasttimerunning		=None
-					nextrun				=None
-					firstTime			=True
-					stop				=False
-				
-			class SVC:
-				def __init__(self, modulos):
-					self.moulos = modulos
-					init= self.Config.getboolean("SVC","svc_init")
-					init_time			=None
-					delay				=float(self.Config.get("SVC","delay"))
-					keepAlive			= True
-					lasttimerunning		=None
-					nextrun				=None
-					firstTime			=True
-					stop				=False
+		
+		 
+			self.host=Controle.Config_ENV.get("MYSQL_R","host")
+			self.user=Controle.Config_ENV.get("MYSQL_R","user")
+			self.passwd=Controle.Config_ENV.get("MYSQL_R","passwd")
+			self.database=Controle.Config_ENV.get("MYSQL_R","database")
+			self.raise_on_warnings=Controle.Config_ENV.get("MYSQL_R","raise_on_warnings")
+		
 			
-			class SDU:
-				def __init__(self, modulos):
-					self.moulos = modulos
-					init= self.Config.getboolean("SDU","sdu_init")
-					init_time			=None
-					delay				=None
-					keepAlive			= True
-					lasttimerunning		=None
-					nextrun				=None
-					firstTime			=True
-					stop				=False
-				
-			class SRC:
-				def __init__(self, modulos):
-					self.moulos = modulos
-					init				= self.Config.getboolean("SRC","src_init")
-					init_time			= None
-					delay				= float(self.Config.get("SRC","delay"))
-					keepAlive			= True
-					lasttimerunning		=None
-					nextrun				=None
-					firstTime			=True
-					stop				=False
+					
+	class MYSQL_W(object):
+		def __init__(self,Controle):
 			
+			self.host=Controle.Config_ENV.get("MYSQL_W","host")
+			self.user=Controle.Config_ENV.get("MYSQL_W","user")
+			self.passwd=Controle.Config_ENV.get("MYSQL_W","passwd")
+			self.database=Controle.Config_ENV.get("MYSQL_W","database")
+			self.raise_on_warnings=Controle.Config_ENV.get("MYSQL_W","raise_on_warnings")
+	
+class API(Controle):
+	def __init__(self,Controle):
+	
+
+		self.mandril = self.mandrill(Controle)
+		self.hubd = self.hubd(Controle)
+		self.soa = self.soa(Controle)
+		self.comtele = self.comtele(Controle)
+		
+	class mandrill:
+		def __init__(self,Controle):
+		
+			self.api_key=Controle.Config_ENV.get("MANDRILL","api_key")
+		
+	class hubd:
+		def __init__(self,Controle):
+				self.url=Controle.Config_ENV.get("HUBD","url")
+				self.api_key=Controle.Config_ENV.get("HUBD","api_key")
+	
+	class soa:
+		def __init__(self,Controle):
+			self.url=Controle.Config_ENV.get("SOA","url")
+			self.user=Controle.Config_ENV.get("SOA","user")
+			self.key=Controle.Config_ENV.get("SOA","key")
+			
+	class comtele:
+		def __init__(self,Controle):
+			self.api_key=Controle.Config_ENV.get("COMTELE","api_key")
+		
+class LINK(Controle):
+	def __init__(self,Controle):
+	
+		self.link_site=Controle.Config_ENV.get("LINK","link_site")
+		self.link_de_compra=Controle.Config_ENV.get("LINK","link_de_compra")
+		self.contact_mail=Controle.Config_ENV.get("LINK","contact_mail")
 			
 		
+	
+class logs(Controle):
+
+	def __init__(self,Controle):
+
+		self.manager_log=Controle.Config.get("LOGS","manager_log")
+		self.sdu_log=Controle.Config.get("LOGS","sdu_log")
+		self.svc_log=Controle.Config.get("LOGS","svc_log")
+		self.sms_log=Controle.Config.get("LOGS","sms_log")
+		self.api_log=Controle.Config.get("LOGS","api_log")
+		self.startup_log=Controle.Config.get("LOGS","startup_log")
+		self.watch_log=Controle.Config.get("LOGS","watch_log")
+	
+class files(Controle):
+	def __init__(self,Controle):
+
+		self.query=Controle.Config.get("FILES","query")
+		self.responses=Controle.Config.get("FILES","responses")
+		self.responses_api=Controle.Config.get("FILES","responses_api")
+		self.responses_sms=Controle.Config.get("FILES","responses_sms")
+
+class modulos(Controle):
+	def __init__(self,Controle):
+
+		self.SMS = self.SMS(Controle)
+		self.SVC = self.SVC(Controle)
+		self.SDU = self.SDU(Controle)
+		self.SRC = self.SRC(Controle)
+
+	class SMS:
+		def __init__(self,Controle):
+		  
+				
+			self.init				= Controle.Config.getboolean("SMS","sms_init")
+			self.init_time			=None
+			self.delay				=None
+			self.keepAlive			= True
+			self.lasttimerunning		=None
+			self.nextrun				=None
+			self.firstTime			=True
+			self.stop				=False
+			
+	class SVC:
+		def __init__(self,Controle):
+		
+			self.init= Controle.Config.getboolean("SVC","svc_init")
+			self.init_time			=None
+			self.delay				=float(Controle.Config.get("SVC","delay"))
+			self.keepAlive			= True
+			self.lasttimerunning		=None
+			self.nextrun				=None
+			self.firstTime			=True
+			self.stop				=False
+	
+	class SDU:
+			
+		def __init__(self,Controle):
+			self.init= Controle.Config.getboolean("SDU","sdu_init")
+			self.init_time			=None
+			self.delay				=None
+			self.keepAlive			= True
+			self.lasttimerunning		=None
+			self.nextrun				=None
+			self.firstTime			=True
+			self.stop				=False
+			
+	class SRC:
+		
+		def __init__(self,Controle):
+			self.init				= Controle.Config.getboolean("SRC","src_init")
+			self.init_time			= None
+			self.delay				= float(Controle.Config.get("SRC","delay"))
+			self.keepAlive			= True
+			self.lasttimerunning		=None
+			self.nextrun				=None
+			self.firstTime			=True
+			self.stop				=False
+
+ 
