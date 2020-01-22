06aff18d321d6e2904d56b9bb51fc10198566ad3
sabbajohn <victorsabba@gmail.com>
2020-01-22 18:02:42 -0300

    Essa linha estava horrivel!

--- services/SDU/DataUpdate.py
+++ services/SDU/DataUpdate.py
@@ -104,7 +104,7 @@ class DataUpdate(object):
 		if result > 0:
 			try:
 
-				os.system("mv {0} {1}/queries/query_old-".format(self.sdu_files.query, self.Manager.Controle.Key.root)+str(agora.hour)+":"+str(agora.minute)+".txt ")
+				os.system("mv {0} {1}/queries/query_old-{2}:{3}.txt".format(self.sdu_files.query, self.Manager.Controle.Key.root, str(agora.hour), str(agora.minute)))
 				os.system("touch {0}".format(self.sdu_files.query))
 			except Exception as e:
 				message = []
