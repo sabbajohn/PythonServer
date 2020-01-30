741e0c12d3108d5702476de0fffbaef0e03036d5
sabbajohn <victorsabba@gmail.com>
2020-01-30 15:31:58 -0300

    Organizando os metodos por ordem de execução

--- services/SVC/servicodevalidacao.py
+++ services/SVC/servicodevalidacao.py
@@ -23,7 +23,197 @@ import ctypes
 
 class servicoDeValidacao(object):
 
+	def __init__(self, M):
 	
+		self.failsafe_tasks=[]
+		self.failsafe_cpf = []
+		self.responses = []
+		self.pendentes_f = []
+		self.result=None	
+		self.Manager = M
+		self.database = self.Manager.database
+		self.svc_files = self.Manager.getControle('files')
+		self.svc_conf = self.Manager.getControle('svc')
+		self.svc_api = self.Manager.getControle('api')
+		self.contador_failsafe = self.svc_api.soa.consultas
+		self.contador_hd =self.svc_api.hubd.consultas
+		self.contador_dispensadas = 0
+		self.contador_ViaCep = self.svc_api.viacep.consultas
+
+	def start(self):
+		self.svc_conf.lasttimerunning = datetime.datetime.now()
+		self.feedback(metodo="start",status=-1,message ='Inicializando serviço de Validação de cadastros...')
+		
+		executor = concurrent.futures.ThreadPoolExecutor(
+		max_workers=1,
+		)
+		self.event_loop = asyncio.new_event_loop()
+
+		start_time = time.time()
+
+		loop = asyncio.new_event_loop()
+		
+		self.pendentes=loop.run_until_complete(self.list_generator(self.database))
+		if not len(self.pendentes)>0:
+			message = []
+			message.append("Finalizando graciosamente")
+			self.feedback(metodo="start",status=0,message = message)
+			message = None
+			return 
+		try:
+			self.event_loop.run_until_complete(
+				self.runner(executor)
+			)
+		finally:
+			self.event_loop.close()
+
+			duration = time.time() - start_time
+			
+			
+			message = []
+			message.append("Foram efetuadas {0} requisições à API Soawebservices".format(self.svc_api.soa.consultas - self.contador_failsafe))
+			message.append("Foram efetuadas {0} requisições à API HubdoDesenvolvedor".format(self.svc_api.hubd.consultas - self.contador_hd))
+			message.append("Foram dispensados {0} registros da validação online por falta de parametros".format(self.contador_dispensadas))
+			message.append("Foram realizadas {0} ao Via Cep".format(self.svc_api.viacep.consultas - self.contador_ViaCep))
+			message.append(f"Total de {len(self.pendentes['pendentes'])} dados consultados em {duration} seconds") 
+			self.Manager.configFile()
+			
+			
+
+		
+			message.append("Encerrando serviço.")
+			self.feedback(metodo="start",status=0,message = message)
+			message = None
+			sleep(15)
+
+	async def list_generator(self,database):
+		self.result, self.contador_dispensadas
+		message = []
+		message.append("Buscando registros pendentes na base de dados. Aguarde!")
+		self.feedback(metodo ='list_generator', status =5, message=message)
+		message = None
+		query= self.svc_conf.query
+		try:
+			self.result=database.execute("R", query)
+		except Exception as e:
+			message = []
+			message.append(type(e))
+			message.append(e)
+			self.feedback(metodo ='list_generator', status =3, message=message)
+			message = None
+			
+		if len(self.result) > 0:
+			message = []
+			message.append('{0} itens serão analisados.'.format(len(self.result)))
+			self.feedback(metodo ='list_generator', status =5, message=message)
+			message = None
+		else:
+			message = []
+			message.append('Não há itens pendentes no momento!')
+			message.append("Encerrando serviço.")
+			self.feedback(metodo ='list_generator', status =0, message=message)
+			message = None
+			return []
+			
+			
+
+		
+		lista = {}
+		lista['pendentes']={}
+		i = 0
+		for x in self.result:
+
+			if x[0]!= None and x[1]!=None:
+				
+					
+					
+				if len(x[0]) > 11:
+					checa_cpfcnpj = cpf.isCnpjValid(x[0])
+				else:
+					checa_cpfcnpj = cpf.isCpfValid(x[0])
+
+				if checa_cpfcnpj==True:
+					lista['pendentes'][i]="https://ws.hubdodesenvolvedor.com.br/v2/cpf/?cpf={0}&data={1}&token=74621930qftLuSWHmA134727632".format(x[0], x[1].strftime("%d/%m/%Y"))
+				else:
+					
+					data = {}
+					data['status'] = False
+					data['id'] = x[2]
+					data['code'] = 1
+					data['message'] ="Cliente {0} não foi validado pois o CPF/CNPJ: {1} está incorreto ".format(x[2],x[0])
+					if ((x[4] == "" or x[4] == None) and (x[5] == "" or x[5] == None)) and (x[6] !="" and x[6] !=None):
+						data['viaCep']=True
+						data['CEP'] = x[6]
+					else:
+						data['viaCep']=False
+					self.contador_dispensadas = self.contador_dispensadas+1 
+					await self.query_generator(data)
+			else:
+				if x[0]== None:
+					data = {}
+					data['status'] = False
+					data['id'] = x[2]
+					data['code'] = 2
+					data['message'] ='Cliente {0} não foi validado pois o CPF/CNPJ está em branco'.format(x[2])
+					if ((x[4] == "" or x[4] == None) and (x[5] == "" or x[5] == None)) and (x[6] !="" and x[6] !=None):
+						data['viaCep']=True
+						data['CEP'] = x[6]
+					else:
+						data['viaCep']=False
+					self.contador_dispensadas = self.contador_dispensadas+1 
+					await self.query_generator(data)
+					
+				else:	
+					if( x[1]== None and (x[3]==None or x[3]=="" )):
+						
+						params={}
+						params['CPF']=x[0]
+						if ((x[4] == "" or x[4] == None) and (x[5] == "" or x[5] == None)) and (x[6] !="" and x[6] !=None):
+							params['viaCep']=True
+							params['CEP'] = x[6]
+						else:
+							params['viaCep']=False
+
+						await self.failsafe_api_validation_request(params)
+					else:	
+						if	x[1]== None:
+							data = {}
+							data['status'] = False
+							data['id'] = x[2]
+							data['code'] = 3
+							data['message'] ="Cliente {0} não foi validado pois o campo data de nascimento está em branco".format(x[2])
+							if ((x[4] == "" or x[4] == None) and (x[5] == "" or x[5] == None)) and (x[6] !="" and x[6] !=None):
+								data['viaCep']=True
+								data['CEP'] = x[6]
+							else:
+								data['viaCep']=False
+							self.contador_dispensadas = self.contador_dispensadas+1 
+							await self.query_generator(data)
+			i = i+1		
+		
+		return lista 	
+
+	async def runner(self,executor):
+		message = []
+		message.append('[INFO]:Iniciando Solicitações a API hubdodesenvolvedor ')
+		self.feedback(metodo="Runner",status=5,message = message)
+		message = None
+		index = 0
+		tasks=[]
+		loop = asyncio.new_event_loop()
+		async with aiohttp.ClientSession() as session:
+			with concurrent.futures.ThreadPoolExecutor() as pool:
+				for i in self.pendentes['pendentes']:
+					url = self.pendentes['pendentes'][i]
+					task = asyncio.ensure_future(self.api_validation_request(session, url, i))
+					tasks.append(task)
+				
+
+				await asyncio.gather(*tasks, return_exceptions=True)
+			
+				message = []
+				message.append( '[INFO]:	Exiting ')
+				self.feedback(metodo="Runner",status=0,message = message)
 
 	async def viaCEP(self,CEP):
 
@@ -34,7 +224,6 @@ class servicoDeValidacao(object):
 				endereco =json.loads(endereco)
 				self.svc_api.viacep.consultas += 1
 				return endereco
-
 
 	async def api_validation_request(self,session, url,index):
 		async with session.get(url) as response:
@@ -53,7 +242,6 @@ class servicoDeValidacao(object):
 			
 			else:
 				pass
-
 
 	async def failsafe_api_validation_request(self,params):
 		message = []
@@ -381,183 +569,26 @@ class servicoDeValidacao(object):
 							else:
 								pass
 
+	def get_id(self): 
+		
+		# returns id of the respective thread 
+		if hasattr(self, '_thread_id'): 
+			return self._thread_id 
+		for id, thread in threading._active.items(): 
+			if thread is self: 
+				return id
 
-	async def runner(self,executor):
+	def raise_exception(self): 
 		message = []
-		message.append('[INFO]:Iniciando Solicitações a API hubdodesenvolvedor ')
-		self.feedback(metodo="Runner",status=5,message = message)
+		message.append( "Serviço finalizado via Watcher")
+		self.feedback(metodo="Watcher", status =5, message = message, erro = False, comments = "Finalizado via Watcher" )
 		message = None
-		index = 0
-		tasks=[]
-		loop = asyncio.new_event_loop()
-		async with aiohttp.ClientSession() as session:
-			with concurrent.futures.ThreadPoolExecutor() as pool:
-				for i in self.pendentes['pendentes']:
-					url = self.pendentes['pendentes'][i]
-					task = asyncio.ensure_future(self.api_validation_request(session, url, i))
-					tasks.append(task)
-				
-
-				await asyncio.gather(*tasks, return_exceptions=True)
-			
-				message = []
-				message.append( '[INFO]:	Exiting ')
-				self.feedback(metodo="Runner",status=0,message = message)
-					
-
-	async def list_generator(self,database):
-		self.result, self.contador_dispensadas
-		message = []
-		message.append("Buscando registros pendentes na base de dados. Aguarde!")
-		self.feedback(metodo ='list_generator', status =5, message=message)
-		message = None
-		query= self.svc_conf.query
-		try:
-			self.result=database.execute("R", query)
-		except Exception as e:
-			message = []
-			message.append(type(e))
-			message.append(e)
-			self.feedback(metodo ='list_generator', status =3, message=message)
-			message = None
-			
-		if len(self.result) > 0:
-			message = []
-			message.append('{0} itens serão analisados.'.format(len(self.result)))
-			self.feedback(metodo ='list_generator', status =5, message=message)
-			message = None
-		else:
-			message = []
-			message.append('Não há itens pendentes no momento!')
-			message.append("Encerrando serviço.")
-			self.feedback(metodo ='list_generator', status =0, message=message)
-			message = None
-			return []
-			
-			
-
-		
-		lista = {}
-		lista['pendentes']={}
-		i = 0
-		for x in self.result:
-
-			if x[0]!= None and x[1]!=None:
-				
-					
-					
-				if len(x[0]) > 11:
-					checa_cpfcnpj = cpf.isCnpjValid(x[0])
-				else:
-					checa_cpfcnpj = cpf.isCpfValid(x[0])
-
-				if checa_cpfcnpj==True:
-					lista['pendentes'][i]="https://ws.hubdodesenvolvedor.com.br/v2/cpf/?cpf={0}&data={1}&token=74621930qftLuSWHmA134727632".format(x[0], x[1].strftime("%d/%m/%Y"))
-				else:
-					
-					data = {}
-					data['status'] = False
-					data['id'] = x[2]
-					data['code'] = 1
-					data['message'] ="Cliente {0} não foi validado pois o CPF/CNPJ: {1} está incorreto ".format(x[2],x[0])
-					if ((x[4] == "" or x[4] == None) and (x[5] == "" or x[5] == None)) and (x[6] !="" and x[6] !=None):
-						data['viaCep']=True
-						data['CEP'] = x[6]
-					else:
-						data['viaCep']=False
-					self.contador_dispensadas = self.contador_dispensadas+1 
-					await self.query_generator(data)
-			else:
-				if x[0]== None:
-					data = {}
-					data['status'] = False
-					data['id'] = x[2]
-					data['code'] = 2
-					data['message'] ='Cliente {0} não foi validado pois o CPF/CNPJ está em branco'.format(x[2])
-					if ((x[4] == "" or x[4] == None) and (x[5] == "" or x[5] == None)) and (x[6] !="" and x[6] !=None):
-						data['viaCep']=True
-						data['CEP'] = x[6]
-					else:
-						data['viaCep']=False
-					self.contador_dispensadas = self.contador_dispensadas+1 
-					await self.query_generator(data)
-					
-				else:	
-					if( x[1]== None and (x[3]==None or x[3]=="" )):
-						
-						params={}
-						params['CPF']=x[0]
-						if ((x[4] == "" or x[4] == None) and (x[5] == "" or x[5] == None)) and (x[6] !="" and x[6] !=None):
-							params['viaCep']=True
-							params['CEP'] = x[6]
-						else:
-							params['viaCep']=False
-
-						await self.failsafe_api_validation_request(params)
-					else:	
-						if	x[1]== None:
-							data = {}
-							data['status'] = False
-							data['id'] = x[2]
-							data['code'] = 3
-							data['message'] ="Cliente {0} não foi validado pois o campo data de nascimento está em branco".format(x[2])
-							if ((x[4] == "" or x[4] == None) and (x[5] == "" or x[5] == None)) and (x[6] !="" and x[6] !=None):
-								data['viaCep']=True
-								data['CEP'] = x[6]
-							else:
-								data['viaCep']=False
-							self.contador_dispensadas = self.contador_dispensadas+1 
-							await self.query_generator(data)
-			i = i+1		
-		
-		return lista 	
-
-	def start(self):
-		self.svc_conf.lasttimerunning = datetime.datetime.now()
-		self.feedback(metodo="start",status=-1,message ='Inicializando serviço de Validação de cadastros...')
-		
-		executor = concurrent.futures.ThreadPoolExecutor(
-		max_workers=1,
-		)
-		self.event_loop = asyncio.new_event_loop()
-
-		start_time = time.time()
-
-		loop = asyncio.new_event_loop()
-		
-		self.pendentes=loop.run_until_complete(self.list_generator(self.database))
-		if not len(self.pendentes)>0:
-			message = []
-			message.append("Finalizando graciosamente")
-			self.feedback(metodo="start",status=0,message = message)
-			message = None
-			return 
-		try:
-			self.event_loop.run_until_complete(
-				self.runner(executor)
-			)
-		finally:
-			self.event_loop.close()
-
-			duration = time.time() - start_time
-			
-			
-			message = []
-			message.append("Foram efetuadas {0} requisições à API Soawebservices".format(self.svc_api.soa.consultas - self.contador_failsafe))
-			message.append("Foram efetuadas {0} requisições à API HubdoDesenvolvedor".format(self.svc_api.hubd.consultas - self.contador_hd))
-			message.append("Foram dispensados {0} registros da validação online por falta de parametros".format(self.contador_dispensadas))
-			message.append("Foram realizadas {0} ao Via Cep".format(self.svc_api.viacep.consultas - self.contador_ViaCep))
-			message.append(f"Total de {len(self.pendentes['pendentes'])} dados consultados em {duration} seconds") 
-			self.Manager.configFile()
-			
-			
-
-		
-			message.append("Encerrando serviço.")
-			self.feedback(metodo="start",status=0,message = message)
-			message = None
-			sleep(15)
-		
+		thread_id = self.get_id() 
+		res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 
+				ctypes.py_object(SystemExit)) 
+		if res > 1: 
+			ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0) 
+			print('Finalizando Serviço')
 
 	def feedback(self,*args, **kwargst):
 		message = kwargst.get('message')
@@ -608,42 +639,3 @@ class servicoDeValidacao(object):
 		feedback['time'] = str(datetime.datetime.now())
 		#with self._lock:
 		self.Manager.callback(feedback)
-
-	def __init__(self, M):
-		self._stop_event = threading.Event()
-		self._lock =threading.Lock()
-		self.failsafe_tasks=[]
-		self.failsafe_cpf = []
-		self.responses = []
-		self.pendentes_f = []
-		self.result=None	
-		self.Manager = M
-		self.database = self.Manager.database
-		self.svc_files = self.Manager.getControle('files')
-		self.svc_conf = self.Manager.getControle('svc')
-		self.svc_api = self.Manager.getControle('api')
-		self.contador_failsafe = self.svc_api.soa.consultas
-		self.contador_hd =self.svc_api.hubd.consultas
-		self.contador_dispensadas = 0
-		self.contador_ViaCep = self.svc_api.viacep.consultas
-	
-	def get_id(self): 
-		
-		# returns id of the respective thread 
-		if hasattr(self, '_thread_id'): 
-			return self._thread_id 
-		for id, thread in threading._active.items(): 
-			if thread is self: 
-				return id
-		
-	def raise_exception(self): 
-		message = []
-		message.append( "Serviço finalizado via Watcher")
-		self.feedback(metodo="Watcher", status =5, message = message, erro = False, comments = "Finalizado via Watcher" )
-		message = None
-		thread_id = self.get_id() 
-		res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 
-				ctypes.py_object(SystemExit)) 
-		if res > 1: 
-			ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0) 
-			print('Finalizando Serviço')
