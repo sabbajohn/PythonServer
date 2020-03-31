238de56a04133b5f6f79a49f783d21b496893fe1
sabbajohn <victorsabba@gmail.com>
2020-03-31 18:09:27 -0300

    Mercado pagop

--- services/SRC/recupecaoDeCarrinhos.py
+++ services/SRC/recupecaoDeCarrinhos.py
@@ -14,6 +14,7 @@ import configparser
 import requests
 import random
 from utils.Gateways.Digimais import Digimais
+from utils.Gateways.MercadoPago import MercadoPago
 from utils.Mandrill.Mandrill import Mandrill
 from utils import funcoes
 class recuperacaoDeCarrinhos(object):
@@ -54,9 +55,6 @@ class recuperacaoDeCarrinhos(object):
 			self.feedback(metodo="start", status =4, message = message, erro = True, comments = "Algo não panejado" )
 			message = None
 		return
-		
-
-		
 
 	async def runNow(self):
 		message = []
@@ -118,10 +116,10 @@ class recuperacaoDeCarrinhos(object):
 			result = None
 			
 			
-			result = self.database.execute("R",self.Manager.SRC_info['query'][0])
+			result = self.database.execute("R",self.Manager.SRC_info['query'][0],serialized = True)
 		
 			if len(result)>0:
-			
+				Mandrill = Mandrill(self.Manager)
 				if(escreveu == True):
 					message = []
 					message.append( "Novos carrinhos encontrados!")
@@ -132,8 +130,8 @@ class recuperacaoDeCarrinhos(object):
 				message.append( "{0} Carrinhos a serem Resgatados!".format(len(result)))
 				self.feedback(metodo="Monitor", status =5, message = message, erro = False )
 				message = None
-				params = self.emailParams(result)
-				self.send(params)
+				Mandrill.send(result)
+				Mandrill = None
 				return
 				
 			else: 
@@ -154,107 +152,6 @@ class recuperacaoDeCarrinhos(object):
 			message = None
 		finally:
 			return
-
-	def checkAPI(self):
-		if self.mandrill_client is None:
-			try:
-				self.mandrill_client = mandrill.Mandrill(self.Manager.MANDRILL_info['api_key'])
-				return True
-			except Exception as e:
-				message = []
-				message.append(type(e))
-				message.append(e)
-				self.feedback(metodo="Monitor", status =2, message = message, erro = True )
-				message = None
-				
-		else:
-			try:
-				valida= self.mandrill_client.users.ping()
-				if "PONG" in valida:
-					return True
-			except:
-				try:
-					self.mandrill_client = mandrill.Mandrill(self.Manager.MANDRILL_info['api_key'])
-					return True
-				except Exception as e :
-					message = []
-					message.append(type(e))
-					message.append(e)
-					self.feedback(metodo="Monitor", status =2, message = message, erro = True )
-					message = None
-					return False
-
-	def emailParams(self, result):
-		cont = 0
-		to = []
-		merge_vars = []
-		keys_to = [ "email","name","VlBilhete"]
-		template_content =  [{'content': 'example content', 'name': 'example name'}]# faço nem ideia do que seja isso
-		global_merge_vars=  [{'content':  self.Manager.LINK_info['link_site'], 'name': 'link_site'},{'content': self.Manager.LINK_info['contact_mail'], 'name': 'CONTACT_MAIL'},{'content':  self.Manager.LINK_info['link_de_compra'], 'name': 'link_de_compra'}]
-		for x in result:
-			nome = x[1].split(" ",1)
-			vlbilhete = format(x[2], '.2f').replace(".",",")
-			merge_vars.append({'rcpt':x[0],'vars': [{'content': nome[0], 'name':'Nome'},{'content':vlbilhete, 'name':'VlBilhete'}]})
-
-			to.append(dict(zip(keys_to, x)))
-			cont +=1
-
-		
-		message = {
-			'global_merge_vars':global_merge_vars,
-			'to': to,
-			'merge_vars':merge_vars,
-			'track_clicks': True,
-			'track_opens': True
-			
-			
-		}
-		return {
-			"template_content":template_content,
-			"message":message,
-			"cont":cont
-		}
-
-	def send(self, p):
-		
-		if(self.checkAPI()):
-			try:
-				result = self.mandrill_client.messages.send_template(template_name='carrinhos-recuperados', template_content=p['template_content'], message=p['message'], ip_pool='Main Pool')
-				if 'queued' in result[0]["status"] or 'sent' in result[0]["status"] :
-					self.Manager.MANDRILL_info['enviados'] += p['cont'] 
-					
-						
-					
-					messages = []
-					messages.append("{0} email's foram enviados".format(p['cont'] ))
-					self.feedback(metodo="send", status =5, message = messages, erro = True, comments = "Email's de recuperação de carrinho" )
-					messages = None
-					
-					return True
-			except mandrill.Error as e:
-				
-				
-				messages = []
-				messages.append( type(e))
-				messages.append(e)
-				self.feedback(metodo="send", status =1, message = messages, erro = True, comments = "Provavelmente algum erro no mandrill" )
-				messages = None
-				
-				return False
-	
-			except Exception as e:
-				messages = []
-				messages.append( type(e))
-				messages.append(e)
-				self.feedback(metodo="send", status =1, message = messages, erro = True)
-				messages = None
-				return False
-		else:
-				messages = []
-				messages.append( "Não foi Possivel validara a Chave API")
-				self.feedback(metodo="send", status =1, message = messages, erro = True, comments = "Chave Mandrill" )
-				messages = None
-				return False
 
 	def recCarrinho2(self,*args, **kwargst):
 		message = []
@@ -317,193 +214,22 @@ class recuperacaoDeCarrinhos(object):
 							carrinhos[i] = carrinho
 							funcoes.saveGoogleLog(carrinho)
 					else:
-						return
+						Gate = MercadoPago(self.Manager)
+						boleto = Gate.boleto(carrinho)
+						if boleto:
+							carrinho['boleto'] = boleto
+							carrinhos[i] = carrinho
+							funcoes.saveGoogleLog(carrinho)
 
 				Mandrill = Mandrill(self.Manager)
 				Mandrill.send('boleto', carrinhos)
+				Mandrill = None
 				return 
 		except Exception as e:
 			message = []
 			message.append( "{0} ".format(len(e)))
 			self.feedback(metodo="recCarrinho2", status =2, message = message, erro = False )
 			message = None
-
-	def geraBoletoDigimais(self,data,carrinho):
-		
-		
-		self.Manager.DIGIMAIS_info['boletos_gerados'] += 1
-		boleto = {
-			"amount"                 => data['valor_boleto'],
-			"invoiceTemplateUUID"    => invoiceTemplateUUID,
-			"originUUID"             => cliente["digimais_id"],
-			"transactionUUID"        => $transactionUUID,
-			"destinationUUID"        => $destinationUUID,
-			"dateDue"                => date("Y-m-d", strtotime("5 weekdays")),
-			"transactionType"        => 1,
-			"callBackInvoiceInfoURL" => $is_online_server ? "https://www.megasorte.com/gateway/notifications_digimais" : "https://webhook.site/28100f89-bb10-4fd3-afd7-8edb0c1c1130",
-			"softDescriptor"         => "Mega Sorte - Título de Capitalização"
-		}
-		body = json.dumps(data).encode('utf8')
-		url = "{}access_token={}".format(self.Manager.MP_info['url'],self.Manager.MP_info['api_key'])
-		
-		response = requests.post(url=url, data=body)
-		status = response.status_code
-		#response =   r.read()
-		response=json.loads(response.text)
-		if status > 204 or status < 200:
-				message = []
-				message.append( "Não foi possivel gerar Boleto. Status {}".format(status))
-				self.feedback(metodo="Gera Boleto", status =5, message = message, erro = False )
-				message = None
-		else:
-			pass
-			self.Manager.MP_info['boletos_gerados'] += 1
-			# Registra boleto no banco de Dados
-			vencimento = response['date_of_expiration'].split("T")[0]
-			query = "INSERT INTO `megasorte`.`boleto_mp` (`gateway_payment_id`, `id_cliente`, `valor`, `vencimento`, `nosso_numero`, `linha_digitavel`, `link_mp`, `id_status`) VALUES ({0}, {1}, {2}, '{3}', '{4}', '{5}', '{6}', 0)".format(response['id'],carrinho[1], response['transaction_amount'],vencimento,response['transaction_details']['payment_method_reference_id'],response['barcode']['content'],response['transaction_details']['external_resource_url'])
-			self.database.execute("W",query,commit=True)
-			self.googleLog(carrinho, response)
-			carrinho.append(response['barcode']['content'])
-			carrinho.append(response['transaction_details']['external_resource_url'])
-			return carrinho
-			# retorna dados do boleto 
-
-	def geraBoletoMP(self,data,carrinho):
-		
-		
-		self.Manager.MP_info['boletos_gerados'] += 1
-		body = json.dumps(data).encode('utf8')
-		url = "{}access_token={}".format(self.Manager.MP_info['url'],self.Manager.MP_info['api_key'])
-		
-		response = requests.post(url=url, data=body)
-		status = response.status_code
-		#response =   r.read()
-		response=json.loads(response.text)
-		if status > 204 or status < 200:
-				message = []
-				message.append( "Não foi possivel gerar Boleto. Status {}".format(status))
-				self.feedback(metodo="Gera Boleto", status =5, message = message, erro = False )
-				message = None
-		else:
-			pass
-			self.Manager.MP_info['boletos_gerados'] += 1
-			# Registra boleto no banco de Dados
-			vencimento = response['date_of_expiration'].split("T")[0]
-			query = "INSERT INTO `megasorte`.`boleto_mp` (`gateway_payment_id`, `id_cliente`, `valor`, `vencimento`, `nosso_numero`, `linha_digitavel`, `link_mp`, `id_status`) VALUES ({0}, {1}, {2}, '{3}', '{4}', '{5}', '{6}', 0)".format(response['id'],carrinho[1], response['transaction_amount'],vencimento,response['transaction_details']['payment_method_reference_id'],response['barcode']['content'],response['transaction_details']['external_resource_url'])
-			self.database.execute("W",query,commit=True)
-			self.googleLog(carrinho, response)
-			carrinho.append(response['barcode']['content'])
-			carrinho.append(response['transaction_details']['external_resource_url'])
-			return carrinho
-			# retorna dados do boleto 
-	
-	def emailParams2(self, result):
-		cont = 0
-		to = []
-		merge_vars = []
-		keys_to = [ "email","name","VlBilhete"]
-		template_content =  [{'content': 'example content', 'name': 'example name'}]# faço nem ideia do que seja isso
-		global_merge_vars=  [{'content':  self.Manager.LINK_info['link_site'], 'name': 'link_site'},{'content': self.Manager.LINK_info['contact_mail'], 'name': 'CONTACT_MAIL'},{'content':  self.Manager.LINK_info['link_de_compra'], 'name': 'link_de_compra'}]
-		for x in result:
-			nome = x[2].split(" ",1)
-			valor = float(x[16])*float(x[11])
-			valor = format(valor, '.2f').replace(".",",")
-			vlbilhete = format(x[11], '.2f').replace(".",",")
-			merge_vars.append({'rcpt':x[3],'vars': [{'content': nome[0], 'name':'Nome'},{'content':vlbilhete, 'name':'VlBilhete'},{'content':x[17],'name':'linhaDigitavel'},{'content':x[18],'name':'link_boleto'},{'content':valor,'name':'Valor'}]})
-			#É aqui que vou passar raiva, nem lembro o que é isso
-			values_to = []
-			values_to.append(x[3])
-			values_to.append(x[2])
-			values_to.append(x[11])
-
-			to.append(dict(zip(keys_to, values_to)))
-			cont +=1
-
-		
-		message = {
-			'global_merge_vars':global_merge_vars,
-			'to': to,
-			'merge_vars':merge_vars,
-			'track_clicks': True,
-			'track_opens': True
-	
-		}
-		return {
-			"template_content":template_content,
-			"message":message,
-			"cont":cont
-		}
-	
-	def send2(self, p):
-		
-		if(self.checkAPI()):
-			try:
-				result = self.mandrill_client.messages.send_template(template_name='carrinhos-recuperados-adv', template_content=p['template_content'], message=p['message'], ip_pool='Main Pool')
-				if 'queued' in result[0]["status"] or 'sent' in result[0]["status"] :
-					self.Manager.MANDRILL_info['enviados'] += p['cont'] 
-					
-						
-					
-					messages = []
-					messages.append("{0} email's foram enviados".format(p['cont'] ))
-					self.feedback(metodo="send2", status =5, message = messages, erro = True, comments = "Email's de recuperação de carrinho(ADV)" )
-					messages = None
-					
-					return True
-			except mandrill.Error as e:
-				
-				
-				messages = []
-				messages.append( type(e))
-				messages.append(e)
-				self.feedback(metodo="send2", status =1, message = messages, erro = True, comments = "Provavelmente algum erro no mandrill" )
-				messages = None
-				
-				return False
-	
-			except Exception as e:
-				messages = []
-				messages.append( type(e))
-				messages.append(e)
-				self.feedback(metodo="send2", status =1, message = messages, erro = True)
-				messages = None
-				return False
-		else:
-				messages = []
-				messages.append( "Não foi Possivel validara a Chave API")
-				self.feedback(metodo="send", status =1, message = messages, erro = True, comments = "Chave Mandrill" )
-				messages = None
-				return False
-
-	def googleLog(self, carrinho, response):
-		weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
-		today = datetime.datetime.now().weekday()
-		weekday = weekDays[today]
-		log = {
-				"env"  : "production",
-				"fn"   : "SRC-II",
-				"type" : "BOLETO-SRC", 
-				"cliente":{
-					"sexo":carrinho[13],
-					"cidade":carrinho[8],
-					"uf":carrinho[9],
-					"nascimento":str(carrinho[14])
-				},
-				"payment":{
-					"payment_method_id":"BOLETO-SRC",
-					"status":"approved",
-					"transaction_amount":response['transaction_amount'],
-					"weekday":weekday,
-					
-				},
-				
-			}
-		with open("/tmp/megasorte-venda.log","a+") as f:
-			l = json.dumps(log)
-			f.write(l)
-			f.write('\n')
-		f.close
-		return
 
 	def feedback(self,*args, **kwargst):
 		message = kwargst.get('message')
